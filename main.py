3532e712d9beca25b0fdcc209c72a248a91f5d26
Howard Jiang <113211442+hwrd22@users.noreply.github.com>
2023-11-28 15:10:32 -0500

    Frontend: Made the Game Over popup window call the difficulty select window instead of just quitting the program, added functionality for backspace input to remove a user-entered number from the board, and removed variable declaration from main.py since a simple OptionsWindow() call starts up pygame immediately.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
     # Any high-level initializations or configurations can go here
     # For now, we'll just start the game
 
-    runtime = OptionsWindow()
+    OptionsWindow()
 
 if __name__ == "__main__":
     main()
