0812a9e8d968fb5e7e893cf20333e6c8a0ae7d91
b-kubick <b.l.kubick@gmail.com>
2023-11-29 17:27:05 -0500

    Ensuring the puzzle has one solution
    Added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from optionsWindow import *
-
+from solver import display_generated_sudoku
 def main():
     # Any high-level initializations or configurations can go here
     # For now, we'll just start the game
-
+    display_generated_sudoku()
     runtime = OptionsWindow()
 
 if __name__ == "__main__":
